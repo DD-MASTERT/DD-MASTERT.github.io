fabbc6cac762014dc8866b92fc1f8225edf1b78e
DD-MASTERT <15737567532@163.com>
2024-07-15 11:44:47 +0800

    Site updated: YYYY-07-2024年7月15日 11:44:44

--- img/1.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# 打开一个图片文件
-with Image.open('2.jpg') as img:
-    # 使用transpose方法，参数Image.FLIP_LEFT_RIGHT实现左右翻转
-    flipped_img = img.transpose(method=Image.FLIP_LEFT_RIGHT)
-    
-    # 保存翻转后的图片
-    flipped_img.save('21.jpg')
